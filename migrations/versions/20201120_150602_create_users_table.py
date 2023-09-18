3bd441e6ee5b854b2ff89e05cfe242fb49be386e
Ledian Fekaj <stroud91official@gmail.com>
2023-09-18 19:04:24 -0400

    Migrations Complete

--- migrations/versions/20201120_150602_create_users_table.py
+++ migrations/versions/20201120_150602_create_users_table.py
@@ -27,10 +27,71 @@ def upgrade():
     sa.Column('username', sa.String(length=40), nullable=False),
     sa.Column('email', sa.String(length=255), nullable=False),
     sa.Column('hashed_password', sa.String(length=255), nullable=False),
+    sa.Column('first_name', sa.String(length=50), nullable=False),
+    sa.Column('last_name', sa.String(length=50), nullable=False),
+    sa.Column('created_at', sa.DateTime(), nullable=False),
+    sa.Column('updated_at', sa.DateTime(), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
+
+    op.create_table('businesses',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('name', sa.String(length=50), nullable=False),
+    sa.Column('address', sa.String(length=255), nullable=False),
+    sa.Column('city', sa.String(length=50), nullable=False),
+    sa.Column('state', sa.String(length=25), nullable=False),
+    sa.Column('zip_code', sa.String(length=10), nullable=False),
+    sa.Column('website', sa.String(length=255), nullable=False),
+    sa.Column('about', sa.String(length=255), nullable=False),
+    sa.Column('phone_number', sa.String(length=30), nullable=False),
+    sa.Column('type', sa.String(length=255), nullable=False),
+    sa.Column('business_image', sa.String(length=255), nullable=False),
+    sa.Column('category_id', sa.Integer(), nullable=False),
+    sa.Column('owner_id', sa.Integer(), nullable=False),
+    # sa.Column('price', sa.String(length=50), nullable=False),
+    # sa.Column('hours_schedule', sa.String(length=25), nullable=False),
+    sa.Column('created_at', sa.DateTime(), nullable=False),
+    sa.Column('updated_at', sa.DateTime(), nullable=False),
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('address'),
+    sa.UniqueConstraint('phone_number'),
+    sa.UniqueConstraint('website'),
+    sa.ForeignKeyConstraint(['owner_id'],['users.id']),
+    sa.ForeignKeyConstraint(['category_id'],['categories.id']),
+    )
+
+    op.create_table('categories',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('name', sa.String(length=40), nullable=False),
+    sa.PrimaryKeyConstraint('id'),
+    )
+
+    op.create_table('reviews',
+    sa.Column('user_id', sa.Integer(), nullable=False),
+    sa.Column('business_id', sa.Integer(), nullable=False),
+    sa.Column('review_body', sa.String(length=255), nullable=False),
+    sa.Column('rating', sa.Integer(), nullable=False),
+    sa.Column('created_at', sa.DateTime(), nullable=False),
+    sa.Column('updated_at', sa.DateTime(), nullable=False),
+    sa.ForeignKeyConstraint(['user_id'],['users.id']),
+    sa.ForeignKeyConstraint(['business_id'],['businesses.id']),
+    sa.PrimaryKeyConstraint('user_id',"business_id"),
+    )
+
+    op.create_table('business_images',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('business_id', sa.Integer(), nullable=False),
+    sa.Column('user_id', sa.Integer(), nullable=False),
+    sa.Column('image_url', sa.String(length=255), nullable=False),
+    sa.Column('create_at', sa.DateTime(), nullable=False),
+    sa.Column('updated_at', sa.DateTime(), nullable=False),
+    sa.PrimaryKeyConstraint('id'),
+    sa.ForeignKeyConstraint(['business_id'], ['businesses.id']),
+    sa.ForeignKeyConstraint(['user_id'], ['users.id'])
+    )
+
 
     if environment == "production":
         op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
