91e1b0de3c24c8fdd716a46d29f52a0334d613b4
gjinhung <gjinhung2@gmail.com>
2023-09-18 23:02:50 -0400

    business_image_seeds

--- app/models/business_image.py
+++ app/models/business_image.py
@@ -7,10 +7,9 @@ class BusinessImage(db.Model):
     business_id = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, nullable=False)
     image_url = db.Column(db.String(255), nullable=False)
-    image_preview = db.Columb(db.Boolean(), nullable=False)
+    image_preview = db.Column(db.Boolean(), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False)
     updated_at = db.Column(db.DateTime(), nullable=False)
-    used = db.Column(db.Boolean(100), nullable=False)
 
     business = db.relationship("Business", back_populates='business_images')
     user = db.relationship("User", back_populates='business_images')
