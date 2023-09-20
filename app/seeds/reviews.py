2d4aba9c6cc23171bac88eaa9ae96fdcf128833c
Ledian Fekaj <stroud91official@gmail.com>
2023-09-20 18:54:22 -0400

    2nd

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -551,8 +551,8 @@ def seed_reviews():
 
 def undo_reviews():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.reviews RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM reviews"))
+        db.session.execute(text("DELETE FROM users"))
 
     db.session.commit()
