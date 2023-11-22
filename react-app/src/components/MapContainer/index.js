4a2df4957c86a965c8e2ad8327abafb4b527932b
Ledian Fekaj <stroud91official@gmail.com>
2023-11-21 21:05:15 -0500

    re commit due to github issue with server

--- react-app/src/components/MapContainer/index.js
+++ react-app/src/components/MapContainer/index.js
@@ -1,37 +1,33 @@
 import React from 'react';
-import dotenv from 'dotenv';
+import './MapContent.css';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-import { Map, Marker, GoogleApiWrapper} from 'google-maps-react';
-
-
-dotenv.config();
-const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-
-
+const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 export function MapContainer({ google, business }) {
-
-
-  const busId= business.id
-
   const mapStyles = {
     width: '300px',
     height: '240px',
   };
 
   return (
-    <Map
-      google={google}
-      zoom={14}
-      style={mapStyles}
-      key={busId}
-      initialCenter={{ lat: business.latitude, lng: business.longitude }}
-    >
-      <Marker position={{ lat: business.latitude, lng: business.longitude }} />
-    </Map>
+    <div className="custom-google-map-container" style={mapStyles}>
+      <Map
+        google={google}
+        zoom={14}
+        style={mapStyles}
+        key={business.id}
+        initialCenter={{
+          lat: business.latitude,
+          lng: business.longitude,
+        }}
+      >
+        <Marker position={{ lat: business.latitude, lng: business.longitude }} />
+      </Map>
+    </div>
   );
 }
 
 export default GoogleApiWrapper({
-  apiKey: API_KEY
+  apiKey: API_KEY,
 })(MapContainer);
