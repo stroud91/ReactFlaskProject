88792182b57849a943797086806d5eeee2bb2712
Ledian Fekaj <stroud91official@gmail.com>
2023-11-22 19:48:22 -0500

    conflict issue for store/index.js

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -4,18 +4,39 @@ import session from './session'
 import bus_images from './images';
 import business from './business'
 import reviews from './review'
-
+// import { persistStore, persistReducer } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
 const rootReducer = combineReducers({
   session,
   business,
   bus_images,
   reviews
 });
-
-
-
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   whitelist: ['business']
+// };
+// const persistedReducer = persistReducer(
+//   persistConfig,
+//   rootReducer
+// );
+// let enhancer;
+// if (process.env.NODE_ENV === 'production') {
+//   enhancer = applyMiddleware(thunk);
+// } else {
+//   const logger = require('redux-logger').default;
+//   const composeEnhancers =
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+// }
+// const configureStore = (preloadedState) => {
+//   let store =  createStore(persistedReducer, preloadedState, enhancer);
+//   let persistor = persistStore(store)
+//   return { store , persistor}
+// };
+// export default configureStore;
 let enhancer;
-
 if (process.env.NODE_ENV === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
@@ -24,14 +45,7 @@ if (process.env.NODE_ENV === 'production') {
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
-
 const configureStore = (preloadedState) => {
-  let store =  createStore(persistedReducer, preloadedState, enhancer);
-  let persistor = persistStore(store)
-  return { store , persistor}
+  return createStore(rootReducer, preloadedState, enhancer);
 };
-
-
-
-
 export default configureStore;
