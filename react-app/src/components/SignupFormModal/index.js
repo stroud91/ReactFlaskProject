a02c7edba3899fe7063e973e699434fad869c59b
Ledian Fekaj <stroud91official@gmail.com>
2024-01-05 21:19:09 -0500

    modified sign up

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { signUp } from "../../store/session";
@@ -17,6 +17,30 @@ function SignupFormModal() {
 	const [errors, setErrors] = useState([]);
 	const { closeModal } = useModal();
 
+	useEffect(() => {
+		const errors = {};
+		if (username.length === 0) errors.username = "";
+		if (email.length === 0) errors.email = "";
+		if (first_name.length === 0) errors.firstName = "";
+		if (last_name.length === 0) errors.lastName = "";
+		if (password.length === 0 || password.length < 6) errors.password = "";
+		if (confirmPassword.length === 0 || confirmPassword.length < 6)
+		  errors.confirmPassword = "";
+		if (username.length < 4 && username.length > 0)
+		  errors.username = "Username needs to be at least 4 characters.";
+		if (password.length < 6 && password.length > 0)
+		  errors.password = "Password needs to be at least 6 characters.";
+
+		setErrors(Object.values(errors));
+	  }, [email, first_name, last_name, username, password, confirmPassword]);
+
+
+	  const isValidEmail = (email) => {
+		const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+		return emailRegex.test(email);
+	  };
+
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		console.log("Username:", username);
@@ -25,7 +49,15 @@ function SignupFormModal() {
 		console.log("First Name:", first_name);
 		console.log("Last Name:", last_name);
 		console.log("Profile Image ID:", profile_image_id);
+
+
 		if (password === confirmPassword) {
+
+            if (!isValidEmail(email)) {
+				setErrors(["Invalid email format"]);
+				return;
+			}
+
 			const formData = new FormData();
 
 			formData.append("username", username);
